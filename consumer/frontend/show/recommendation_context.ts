f766f513c8c7882171a4625d99b18b1e3477c1fb
Yuxuan Xie <teststaybaka@gmail.com>
2024-07-04 22:27:59 -0700

    Add freeForm field to search.

--- consumer/frontend/show/recommendation_context.ts
+++ consumer/frontend/show/recommendation_context.ts
@@ -1,12 +1,17 @@
 import { MessageDescriptor, PrimitiveType } from '@selfage/message/descriptor';
 
 export interface RecommendationContext {
+  freeForm?: string,
   publisherId?: string,
 }
 
 export let RECOMMENDATION_CONTEXT: MessageDescriptor<RecommendationContext> = {
   name: 'RecommendationContext',
   fields: [
+    {
+      name: 'freeForm',
+      primitiveType: PrimitiveType.STRING,
+    },
     {
       name: 'publisherId',
       primitiveType: PrimitiveType.STRING,
